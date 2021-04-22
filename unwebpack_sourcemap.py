df1c73ce9fd134ff648dea9b3f7af13f13a1b032
Arthur A <aires.arthur.15@gmail.com>
2021-04-22 04:29:11 -0300

    Update unwebpack_sourcemap.py
    Sometimes the script tries to write directories, so I modified it before writing so that it doesn't allow directories.

--- unwebpack_sourcemap.py
+++ unwebpack_sourcemap.py
@@ -183,7 +183,12 @@ class SourceMapExtractor(object):
                 write_path = self._get_sanitised_file_path(source)
                 if write_path is not None:
                     os.makedirs(os.path.dirname(write_path), mode=0o755, exist_ok=True)
-                    with open(write_path, 'w', encoding='utf-8', errors='ignore') as f:
+
+                    if '.' not in write_path:
+                        print("Is A Directory", write_path)
+                        continue
+
+                    with open(write_path, 'w', encoding='utf-8', errors='ignore') as f:   
                         print("Writing %s..." % os.path.basename(write_path))
                         f.write(content)
             else:
